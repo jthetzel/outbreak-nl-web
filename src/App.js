f1594545042aea685ca13c5bc00b5ef68ff1f445
jthetzel <jthetzel@gmail.com>
2018-03-27 13:17:53 -0230

    Add google analytics

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
+import ReactGA from 'react-ga'
 import configureStore from './redux/configureStore'
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-// import injectTapEventPlugin from 'react-tap-event-plugin'
-// import ReactGA from 'react-ga'
 import Root from './containers/Root'
 
-// ReactGA.initialize('UA-102217061-1')
-// ReactGA.pageview('/')
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-116513283-1')
+  ReactGA.pageview('/')
+}
 
 const store = configureStore()
-// work around for material-ui tap events
-// injectTapEventPlugin()
 
 class App extends Component {
   render () {
